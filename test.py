2b8f6179956cbc060e8e8c33908e8c6d8971c63d
moidata <77037486+mohammedlili324@users.noreply.github.com>
2022-11-03 17:23:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -31,14 +31,15 @@ def startw():
                 
 
 
-        put_text('startsing')
-        ww = ChromeDriverManager().install()
+        from selenium import webdriver
         options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('disable-gpu')
-        a = webdriver.Chrome(executable_path=ww, chrome_options=options)
-        put_text('hi world')
-
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        # open it, go to a website, and get results
+        wd = webdriver.Chrome('chromedriver',options=options)
+        wd.get("https://www.website.com")
+        put_text(wd.page_source)  # results
 
     
 
